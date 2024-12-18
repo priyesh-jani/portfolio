df6c420a58cbf628d17831574dd746e483fd036a
Priyesh Jani <88060651+priyesh-jani@users.noreply.github.com>
2024-12-17 18:05:32 -0600

    Update main.py

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ def get_predefined_answer(user_question, df, question_vectors):
     return df.iloc[closest_idx]["response"]
 
 # Load distilgpt2 model and tokenizer
-model_name = "facebook/blenderbot-400M-distill"
+model_name = "distilgpt2"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 ##model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
 model = AutoModelForCausalLM.from_pretrained(
